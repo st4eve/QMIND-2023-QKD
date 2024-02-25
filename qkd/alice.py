6ed2d9cc683b41a6fcf03f3bcd621e7f8c7c6cac
Tristan <17tna@queensu.ca>
2024-02-25 16:51:51 -0500

    Still a bug causing measurement to not line up got alice and bob

--- qkd/alice.py
+++ qkd/alice.py
@@ -4,47 +4,61 @@ import copy
 
 
 class Alice:
-    def __init__(self, n):
+    def __init__(self, 
+                 n,
+                 verbose=0):
         self.n = n
-        self.qr = QuantumRegister(n, name = 'qr')
-        self.cr = ClassicalRegister(n, name = 'cr')
-        self.circuit = QuantumCircuit(self.qr, self.cr, name = 'Alice')
-        self.key = np.random.random_integers(0, 1, n)
+        self.circuit = [QuantumCircuit(1,1) for i in range(n)]
+        self.key = np.random.randint(0, 2, size=n)
         self.bases = []
+        self.verbose = verbose
+        
+        
+        self.encode_key()
+        self.switch_bases()
 
     def encode_key(self):
-        for index, digit in enumerate(self.key):
-            if digit == '1':
-                self.circuit.x(self.qr[index])
-        print("Encoded key:", self.key)
+        for i, digit in enumerate(self.key):
+            if digit:
+                self.circuit[i].x(0)
+        if self.verbose:
+            print("Encoded key:", self.key)
 
     def switch_bases(self):
-        for index in range(len(self.qr)):
+        for i, _ in enumerate(self.circuit):
             if 0.5 < np.random.random():
-                self.circuit.h(self.qr[index])
-                self.bases.append('X')
-            else:
+                self.circuit[i].h(0)
                 self.bases.append('H')
-        print("Bases:", self.bases)
+            else:
+                self.bases.append('X')
+        if self.verbose:
+            print("Bases:", self.bases)
 
     def get_circuit(self):
         return copy.deepcopy(self.circuit)
 
     def process_bases(self, bob_bases):
         matching_bases = [i for i, (alice_base, bob_base) in enumerate(zip(self.bases, bob_bases)) if alice_base == bob_base]
-        print("Matching bases:", matching_bases)
+        self.bob_matching_bases = matching_bases
+        if self.verbose:
+            print("Matching bases index:", matching_bases)
+            print('Matching bases values:', [self.bases[i] for i in matching_bases])
+        self.generate_key()
         return matching_bases
 
-    def generate_key(self, matching_bases):
-        generated_key = ''.join([str(self.key[i]) for i in matching_bases])
-        print("Length of Generated key:", len(generated_key))
+    def generate_key(self):
+        generated_key = ''.join([str(self.key[i]) for i in self.bob_matching_bases])
+        if self.verbose:
+            print("Length of Generated key:", len(generated_key))
+            print('Generated key:', generated_key)
         return generated_key
 
-    def get_key_part(self, part_size):
-        key_part = self.key[:part_size]
-        self.key = self.key[part_size:]
-        print("Key part:", key_part)
-        return key_part
+    def get_comparison_key(self, comparisonLength):
+        self.comparison_key = self.key[:comparisonLength]
+        self.key = self.key[comparisonLength:]
+        if self.verbose:
+            print("Key part:", self.comparison_key)
+        return self.comparison_key
 
 
 if __name__ == '__main__':
--- qkd/bob.py
+++ qkd/bob.py
@@ -1,43 +1,50 @@
-from qiskit import Aer, QuantumCircuit
+from qiskit_aer import AerSimulator
+from qiskit import QuantumCircuit
 from numpy.random import randint
+from alice import Alice
+import copy
 
 class Bob():
-    def __init__(self, qc_alice, comparisonLen):
+    def __init__(self, 
+                 qc_alice, 
+                 minComparisonLen):
         # Creates copy of Alice quantum circuit
-        self.qc = qc_alice.copy()   
+        self.qc = copy.deepcopy(qc_alice)
         
         # Determines the number of qubits in quantum circuit
-        self.num_qubits = self.qc.num_qubits()  
+        self.num_qubits = len(self.qc) 
         
         # Checks if the comparison length is more than amount of qubits
-        if self.num_qubits < comparisonLen:
+        if self.num_qubits//4 < minComparisonLen:
             # Throws execption
             raise Exception("Comparison length must be shorter than the message.")  
+        else:
+            self.minComparisonLen = minComparisonLen
         
         # Create bases for bob
-        self.bases = randint(2, size=self.num_qubits)   
+        self.bases = ['X' if randint(0, 2) else 'H' for i in range(self.num_qubits)]
+        self.measure_message(self.qc)
         
     def measure_message(self, message):
         # Creates simulator
-        backend = Aer.get_backend('aer_simulator')
+        simulator = AerSimulator(method='statevector')
         
         # measurement array and number of qubits created
         measurements = []
-        n = message.len()
+        n = self.num_qubits
         
         for q in range(n):
             # If the base is 0, measure in the Z-basis
-            if self.bases[q] == 0:
+            if self.bases[q] == 'X':
                 message[q].measure(0,0)
              # If the base is 1, measure in the X-basis
-            if self.bases[q] == 1:
-                # Applies a Hadamard gate to the qubit to change the basis to the X-basis
+            if self.bases[q] == 'H':
+                # Applies a Hadamard gate to the qubit to change the basis to the Z-basis
                 message[q].h(0)
                 message[q].measure(0,0)
 
             # Runs simulation of the  quantum circuit
-            aer_sim = Aer.get_backend('aer_simulator')  
-            results = aer_sim.run(message[q], shots=1, memory=True).result()
+            results = simulator.run(message[q], shots=10, memory=True).result()
             
             # Gets the results from the simulation and converts it to an integer
             measured_bit = int(results.get_memory()[0])
@@ -47,39 +54,40 @@ class Bob():
 
         self.bobMeasurement = measurements
     
-    def remove_garbagebits(self, aliceBases, comparisonLen):
-        kept_bits = []
-        
-        # Checks if the bases used by alice and bob are the same
-        for i in range(self.bobMeasurement.len()):
-            if aliceBases[i] == self.bases[i]:
-                kept_bits.append(self.bobMeasurement[i])
-        
+    def remove_garbagebits(self, aliceBases):
+
+        kept_bits = [self.bobMeasurement[i] for i in aliceBases]
+        print('Bobs measurements: ', self.bobMeasurement)
+        print("Bobs Kept bits: ", kept_bits)
+        self.comparisonLength = len(kept_bits) - self.num_qubits//4
+        if self.comparisonLength < self.minComparisonLen:
+            raise Exception("Insufficient comparison bits")
         # Stores the key 
-        self.key = kept_bits[comparisonLen:]
+        self.key = kept_bits[self.comparisonLength:]
         
         # Stores the qubits to be compared
-        self.comparisonKey = kept_bits[:comparisonLen]
+        self.comparisonKey = kept_bits[:self.comparisonLength]
         
     def check_eve(self, aliceCompareKey):
         # Compare length of alice comparison key to bob comparison key
-        if aliceCompareKey != self.comparisonKey:
+        if len(aliceCompareKey) != len(self.comparisonKey):
             raise Exception("Comparsion keys are not the same length")
         
         # Counter for number of same bases
         sameBases = 0.0
         
-        for i in range(aliceCompareKey.len()):
+        for i in range(len(aliceCompareKey)):
             # Check is in same bases
-            if aliceCompareKey[i] == self.get_comparisonkey[i]:
+            if aliceCompareKey[i] == self.comparisonKey[i]:
                 sameBases = sameBases + 1
         
         # Calculate percent of same bases
-        percentageSame = (sameBases / aliceCompareKey.len()) * 100
+        percentageSame = (sameBases / len(aliceCompareKey)) * 100
+        print(percentageSame)
         
         # Print message
         if percentageSame < 75.0:
-            print("less than 75% of bases were wrong")    
+            print("More than 25% of the qubits did not match. Eve has tampered with the message.")    
             print("Key is not secured")
         else:
             print("Key is secured")
@@ -96,4 +104,15 @@ class Bob():
     def get_comparisonkey(self):
         return self.comparisonKey
     
-        
+if __name__ == '__main__':
+    print('Bob class file')
+    # Create Alice object
+    alice = Alice(32, verbose=0)
+    # Create Bob object
+    bob = Bob(alice.get_circuit(), 4)
+    print(alice.get_circuit()[0].draw())
+    print(bob.qc[0].draw())
+    same_bases = alice.process_bases(bob.get_bases())
+    bob.remove_garbagebits(same_bases)
+    compare_key = alice.get_comparison_key(bob.comparisonLength)
+    bob.check_eve(compare_key)
